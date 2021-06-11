36fb85437352dce30e3bd8d8c6e6ab8b78a2e7e1
Rocco Chen <jack_6667@yahoo.com.tw>
2021-06-11 20:03:57 +0800

    抽學員

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,14 +7,17 @@ const
   google_file_feed_id = '1cY0YYL67Us5SotGM3L9gIxWkGdRkDc5SuthMNaNGQjg',
   url = `https://spreadsheets.google.com/feeds/list/${google_file_feed_id}/1/public/values?alt=json`,
   duration = 3500; // 拉霸效果執行多久
-var cheer_data, btn = $('.btn-start');
+var member_data, btn = $('.btn-start');
 fetch(url)
   .then(res => res.json())
   .then(data => {
     var all_data = data.feed.entry;
-    cheer_data = all_data.map(d => `${d.gsx$姓名.$t} - ${d.gsx$電話.$t}`).
-      filter(onlyUnique).map(d => `${d.split(" - ")[0]} - ${mask(d.split(" - ")[1])}`);
-    $('body').append(`<style>${spin_style(cheer_data.length)}</style>`);
+    var tmp = {};
+    all_data.forEach(function(o){
+      tmp[`${o.gsx$姓名.$t} - ${o.gsx$電話.$t}`] ||= o.gsx$幫誰加油請輸入全名或勾選無.$t;
+    });
+    member_data = Object.values(tmp)
+    $('body').append(`<style>${spin_style(member_data.length)}</style>`);
     let txt; // 結果
     // 按鈕
     btn.on('click', function(e){
@@ -39,10 +42,10 @@ fetch(url)
         // 移除動畫
         setTimeout(() => {
           Array.prototype.forEach.call(list, l => l.removeAttribute('class'));
-          btn.removeClass('not-allow')
+          btn.removeClass('not-allow');
         }, duration);
       };
-      chooseShop(cheer_data);
+      chooseShop(member_data);
     });
   }
 );
